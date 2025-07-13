b80e5825212ec233b65d21ba4659b463168cc373
Chaouachi hamza <hamza.chaouachi@digitalroguewave.com>
2025-07-13 17:38:30 +0200

    ux updates

--- components/MyRuntimeProvider.tsx
+++ components/MyRuntimeProvider.tsx
@@ -11,7 +11,7 @@ const MyModelAdapter: ChatModelAdapter = {
   async run({ messages, abortSignal }) {
     const firstContent = messages[0].content[0];
     if (firstContent && firstContent.type === "text") {
-      const result = await fetch("https://139.59.135.113:8001/generate", {
+      const result = await fetch("https://quatar-legal-chatbot.duckdns.org/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
