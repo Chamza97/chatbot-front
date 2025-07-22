3ce6af7a9980d35e798cb0aa221e64ca06c4c7e2
Chaouachi Hamza <hamza.chaouachi@digitalroguewave.com>
2025-07-22 23:42:44 +0200

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,12 +4,39 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 
+import { Router, Request, Response, NextFunction, RequestHandler } from 'express';
+import 'reflect-metadata';
 
-export function Controller(prefix: string = '') {
-  return function <T extends new (...args: any[]) => {}>(TargetClass: T): 
-    new (...args: ConstructorParameters<T>) => InstanceType<T> & WithRouter {
-    return class extends TargetClass {
-      private readonly _router = Router();
+type HttpMethod = 'get' | 'post' | 'put' | 'delete' | 'patch';
+
+interface RouteDefinition {
+  method: HttpMethod;
+  path: string;
+  handlerName: string;
+  middlewares: RequestHandler[];
+}
+
+// Simulation de l’injection de paramètres décorés (tu peux l'adapter ensuite)
+function injectParams(
+  handler: (...args: any[]) => any,
+  context: unknown,
+  handlerName: string
+): (...args: [Request, Response, NextFunction]) => Promise<void> {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    return handler(req, res, next);
+  };
+}
+
+// Interface à implémenter pour exposer le router
+export interface IController {
+  readonly router: Router;
+}
+
+// Décorateur @Controller
+export function Controller(prefix = ''): ClassDecorator {
+  return function <T extends new (...args: any[]) => {}>(TargetClass: T): T {
+    return class extends TargetClass implements IController {
+      private readonly _router: Router = Router();
 
       constructor(...args: any[]) {
         super(...args);
@@ -17,25 +44,27 @@ export function Controller(prefix: string = '') {
       }
 
       private registerRoutes(): void {
-        const routes = Reflect.getMetadata('routes', TargetClass) || [];
+        const routes: RouteDefinition[] = Reflect.getMetadata('routes', TargetClass) || [];
+
         routes.forEach(({ method, path, handlerName, middlewares }) => {
           const handler = (this as any)[handlerName].bind(this);
           const handlerWithParams = injectParams(handler, this, handlerName);
+
           this._router[method](
             `${prefix}${path}`,
             ...middlewares,
-            async (req, res, next) => {
+            async (req: Request, res: Response, next: NextFunction) => {
               try {
                 await handlerWithParams(req, res, next);
-              } catch (err) {
-                next(err);
+              } catch (error) {
+                next(error);
               }
             }
           );
         });
       }
 
-      public get router() {
+      public get router(): Router {
         return this._router;
       }
     };
